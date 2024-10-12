f7a04125dd6746bb10eee54f19a5de1272e6f00d
Alberto Arroyo Raygada <beyondnet.peru@gmail.com>
2024-10-12 02:42:25 -0500

    Refactoring

--- src/Services/MyPlanner.Plannings.ApiTests/UseCases/Plan/Queries/PlanQueryRepositoryTests.cs
+++ src/Services/MyPlanner.Plannings.ApiTests/UseCases/Plan/Queries/PlanQueryRepositoryTests.cs
@@ -1,12 +1,4 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using MyPlanner.Plannings.Api.UseCases.Plan.Queries;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MyPlanner.Plannings.Api.UseCases.Plan.Queries.Tests
+﻿namespace MyPlanner.Plannings.Api.UseCases.Plan.Queries.Tests
 {
     [TestClass()]
     public class PlanQueryRepositoryTests
